ef58f8f2e4021c71d5a2facdacfb4b4e74d65798
sinclairstudy <sinclairstudy@gmail.com>
2018-03-27 17:29:14 -0400

    other deaths

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -3,6 +3,8 @@ class Creature < ApplicationRecord
   has_many :results
 
   def eat(edible)
+
+
     edible.eaten = true
     if edible.value > self.size
       self.alive = false
@@ -11,12 +13,36 @@ class Creature < ApplicationRecord
         creature: self,
         location: edible.location,
         cause_of_death: "You bit off more than you can chew. \nDeath by asphyxiation!!!!")
-        self.results << result
-        self.save
+      self.results << result
+      self.save
     else
       self.size += edible.value
+
+      # should tell them they have new abilities when they gain them
       self.fly = true if edible.fly
       self.swim = true if edible.swim
+    end
+  end
+
+  def visit(location)
+    if location.fly && !self.fly
+      self.alive = false
+      result = Result.create(
+        user: self.user,
+        creature: self,
+        location: location,
+        cause_of_death: "You tried to fly to #{location.name}, but you didn't know how, and fell to your death. Tragic.")
+      self.results << result
+      self.save
+    elsif location.swim && !self.swim
+      self.alive = false
+      result = Result.create(
+        user: self.user,
+        creature: self,
+        location: location,
+        cause_of_death: "You have drowned.")
+      self.results << result
+      self.save
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   resources :edibles, only: [:update]
   resources :creatures, only: :create
   resources :results, only: :index
-  # resources :users, only: [:new, :create]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
