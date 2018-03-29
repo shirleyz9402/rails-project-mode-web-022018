ceb21b43de0cb33fbcbb4b702be6b52e9d63d5bf
sinclairstudy <sinclairstudy@gmail.com>
2018-03-29 11:57:00 -0400

    aaaaaay we have one test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326193846) do
+ActiveRecord::Schema.define(version: 20180329154816) do
 
   create_table "creatures", force: :cascade do |t|
     t.integer "user_id"
@@ -71,6 +71,12 @@ ActiveRecord::Schema.define(version: 20180326193846) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "widgets", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/models/creature_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Creature, type: :model do
+
+  it "sdfljkdfjlkjlksdf" do
+    c = Creature.new(name: "Poop")
+    expect(c.name).to eq("Poop")
+  end
+
+
+
+
+
+end
